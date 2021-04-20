eb27d8fcb8f182555f4b54e0873b50ef4e1d61a6
AidanAnderson <aidananderson167@gmail.com>
2021-04-20 15:29:26 -0700

    New MessageBuilder Class which handles message keys and their values

---
+++ OnePlayerSleepPlugin/src/aid/me/ops/util/MessageBuilder.java
@@ -0,0 +1,101 @@
+package aid.me.ops.util;
+
+import java.util.HashMap;
+
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+
+import aid.me.ops.OpsPlugin;
+import aid.me.ops.sleep.SleepManager;
+
+public class MessageBuilder {
+	
+	private ConfigurationManager config;
+	private SleepManager sleepMang;
+	private HashMap<String, String> messageMap;
+	
+	//Constant values
+	private final String[] values = {
+		OpsPlugin.getPlugin().getName(),
+		OpsPlugin.getCommandManager().getCurrPlayer().getName(), 
+		this.buildSleepString(),
+		String.valueOf(config.getEnabled()), 
+		String.valueOf(config.getWeather()), 
+		String.valueOf(config.getDuration())
+	};
+	
+	//Initialization upon construction
+	public MessageBuilder() {
+		this.config = OpsPlugin.getConfigManager();
+		this.sleepMang = OpsPlugin.getSleepManager();
+		this.messageMap = new HashMap<String, String>();
+		this.updateValues();
+	}
+	
+	//Returns a updated messageMap
+	public HashMap<String, String> getMessageMap(){
+		this.updateValues();
+		return this.messageMap;
+	}
+	
+	//Builds a string based on the amount of players that slept
+	public String buildSleepString() {
+		
+		String finalizedText = "";
+		
+		CraftPlayer[] players = 
+				sleepMang.getSleepingPlayers().keySet().toArray(new CraftPlayer[sleepMang.getSleepingPlayers().size()]);
+		
+		if(players.length > 1) {
+			
+			for(int x = 0; x < players.length; x++) {
+				//Check for last player
+				if(x == players.length - 1) {
+					finalizedText.concat("and " + players[x].getName());
+					break;
+				}
+				finalizedText.concat(players[x].getName() + ", ");
+			}
+		}else {
+			finalizedText = players[0].getName();
+		}
+		
+		return finalizedText;
+	}
+	
+	//Logic for updating the messageMap
+	private void updateValues() {
+		
+		final MessageKey[] keys = MessageKey.values();
+		this.messageMap.clear();
+		
+		int x = 0;
+		for(MessageKey key : keys) {
+			if(values[x] == null) {
+				messageMap.put(key.getLabel(), "");
+			}else {
+				messageMap.put(key.getLabel(), values[x]);
+			}
+			x++;
+		}
+	}
+}
+
+enum MessageKey {
+	
+	PLUGIN("{plugin}"),
+	PLAYER("{player}"),
+	SENDER("{sPlayer}"),
+	ENABLED("{enabled}"),
+	WEATHER("{weather}"),
+	DURATION("{duration}");
+	
+	private String label;
+	
+	private MessageKey(String label){
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	}
+}
--- OnePlayerSleepPlugin/src/aid/me/ops/util/MessageManager.java
+++ OnePlayerSleepPlugin/src/aid/me/ops/util/MessageManager.java
@@ -4,74 +4,22 @@ import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 
 import aid.me.ops.OpsPlugin;
-import aid.me.ops.sleep.SleepManager;
 
 public class MessageManager {
 	
-	private ConfigurationManager config = OpsPlugin.getConfigManager();
-	private SleepManager mang = OpsPlugin.getSleepManager();
+	//Class wide variables
+	private MessageBuilder builder;
+	private ConfigurationManager config;
 	
-	private HashMap<String, String> keyTerms = new HashMap<String, String>();
-	
-	private void updateMapValues() {
-		
-		
-		keyTerms.clear();
-		String[] terms = {"{plugin}", "{player}", "{sPlayer}" , "{enabled}", "{weather}", "{duration}"};
-		
-		Object[] vals = 
-			{
-				OpsPlugin.getPlugin().getName(),
-				OpsPlugin.getCommandManager().getCurrPlayer().getName(), 
-				this.getSleepingString(),
-				config.getEnabled(), 
-				config.getWeather(), 
-				config.getDuration()
-			};
-		
-		for(int i = 0; i < terms.length; i++) {
-			if(vals[i] == null) {
-				keyTerms.put(terms[i], "");
-			}
-			
-			keyTerms.put(terms[i], vals[i].toString());
-		}
-		
-		return;
+	//Intialization upon construction
+	public MessageManager() {
+		this.builder = new MessageBuilder();
+		this.config = OpsPlugin.getConfigManager();
 	}
 	
-
-	public String getSleepingString() {
-		
-		String text = "";
-		
-		CraftPlayer[] players = mang.getSleepingPlayers().keySet().toArray(new CraftPlayer[mang.getSleepingPlayers().size()]);
-		
-		if(players.length == 1) {
-			text = players[0].getName();
-			return text;
-		}
-		
-		for(int i = 0; i < players.length; i++) {
-			if(i == players.length - 1) {
-				text = text + players[i].getName();
-				break;
-			}
-			text = text + players[i].getName() + ", ";
-		}
-		
-		return text;
-	}
-
-	
-	public HashMap<String, String> getTermsMap() {
-		this.updateMapValues();
-		return keyTerms;
-	}
-	
+	//Wrapper methods
 	public void broadcastMessage(String path) {
 		Bukkit.getServer().broadcastMessage(formatMessage(getRawMsg(path)));
 	}
@@ -91,7 +39,7 @@ public class MessageManager {
 	
 	public String formatMessage(String msg) {
 		
-		this.updateMapValues();
+		HashMap<String, String> keyTerms = builder.getMessageMap();
 		
 		for(String x : keyTerms.keySet()) {
 			msg = msg.replace(x, keyTerms.get(x));
